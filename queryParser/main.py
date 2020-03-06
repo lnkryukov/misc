8ddcee8f8b499259a63c6758c5c1f806b1c41ba9
vagrant <lnkryukov@miem.hse.ru>
2020-03-06 15:47:44 +0000

    comments

--- queryParser/main.py
+++ queryParser/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_caster import FlaskCaster
+from flask_caster import FlaskCaster # Нужно для автоматического кастования offset и size в int
 from schema import Schema, Optional
 import pprint
 
@@ -7,7 +7,9 @@ pp = pprint.PrettyPrinter().pprint
 
 app = Flask(__name__)
 
+# Нужно обернуть app, чтобы каст работал
 caster = FlaskCaster(app)
+# Параметры size и offset всегда будут int!
 caster.ints = ['size', 'offset']
 
 def parseOffsetSize(args):
@@ -17,7 +19,7 @@ def parseOffsetSize(args):
             Optional('size'): int
         }, ignore_extra_keys=True
     )
-    schema.validate(dict(args))
+    schema.validate(dict(args)) # Схема не умеет валидировать Immutable Dict, поэтому кастуем к обычному
     return (args.get('offset'), args.get('size'))
     
 def getData(offset=0, size=100):
@@ -25,7 +27,7 @@ def getData(offset=0, size=100):
         offset = 0
     if size is None:
         size = 100
-    return list(range(offset + 1, offset + 1 + size))
+    return list(range(offset + 1, offset + 1 + size)) # Имитируем получение данных...
 
 @app.route('/test', methods=['GET'])
 def test():
