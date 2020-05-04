fd2c632c625ecb4f06a65e3c29faf727b9570d6b
vagrant <lnkryukov@miem.hse.ru>
2020-05-04 16:43:17 +0000

    schema

---
+++ schema_validation/test.py
@@ -0,0 +1,33 @@
+# TODO: Замутить issue на тему этого бага
+
+from schema import Schema, Forbidden, SchemaForbiddenKeyError
+
+try:
+    Schema(
+        {
+            Forbidden('test', error='TEST_ERROR'): str
+        }
+    ).validate(
+        {
+            'test': 'value'
+        }
+    )
+except SchemaForbiddenKeyError as e:
+    print(  'Auto: {}\n'
+            'User: {}\n'.format(e.autos, e.errors)
+    )
+
+try:
+    Schema(
+        {
+            Forbidden('test', error='TEST_ERROR'): str
+        }
+    ).validate(
+        {
+            'test': 'value'
+        }
+    )
+except SchemaForbiddenKeyError as e:
+    print(  'Auto: {}\n'
+            'User: {}\n'.format(e.autos, e.errors)
+    )
